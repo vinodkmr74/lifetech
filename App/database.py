7bb4bb98558e4eb88c96eaa9950cd7ac35e9e488
Vinod Kumar <vinod stm74@gmail.com>
2026-01-07 18:54:10 +0530

    new

--- App/database.py
+++ App/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from decouple import config
+# from decouple import config
 from dotenv import load_dotenv
+import os
 
 load_dotenv() 
 
-DATABASE_URL = config("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
