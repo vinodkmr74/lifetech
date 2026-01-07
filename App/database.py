9eaaa8a470bf55746cb2c6d017ab7d40a15e2617
Vinod Kumar <vinod stm74@gmail.com>
2026-01-07 18:18:35 +0530

    new

--- App/database.py
+++ App/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 DATABASE_URL = config("DATABASE_URL")
 
