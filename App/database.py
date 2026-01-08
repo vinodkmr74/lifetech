65c480a6f24090220cc545fa161a8d2094699426
Vinod Kumar <vinod stm74@gmail.com>
2026-01-08 18:53:12 +0530

    new

--- App/database.py
+++ App/database.py
@@ -4,14 +4,18 @@ from sqlalchemy.orm import sessionmaker
 # from decouple import config
 from dotenv import load_dotenv
 import os
+import pymysql
 
+
+
+pymysql.install_as_MySQLdb()
 load_dotenv() 
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True,pool_recycle=280)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
