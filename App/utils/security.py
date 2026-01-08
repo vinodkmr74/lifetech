82ce9b53b63192d5d744f1eea5f47fdf8644746b
Vinod Kumar <vinod stm74@gmail.com>
2026-01-08 21:15:48 +0530

    new

--- App/utils/security.py
+++ App/utils/security.py
@@ -18,16 +18,27 @@
 #     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from App.utils.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
 
+# 🔐 bcrypt max = 72 bytes
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password[:72])
+    password = password[:72]
+    return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password[:72], hashed_password)
+def verify_password(plain: str, hashed: str) -> bool:
+    plain = plain[:72]
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
